1747f7e5481e2340447386f76bc1e8b7702d59e5
Maximillian Dornseif <md@hudora.de>
2011-01-07 23:11:38 +0100

    infrastructure.taskqueue_add_multi() added
    infrastructure.taskqueue_add_multi_payload() for adding tasks with big JSON endoded, compressed payloads
    
    fixed broken Doku
    
    extended `make check` target, fixed code accordingly
    
    off by one error & limits raised in pagination, logging after enqueueing
    
    Change-Id: I862671d7c13e957603486bb1da40f5bfc7e33019
    Reviewed-on: http://gerrit.hudora.biz/259
    Reviewed-by: Maximillian Dornseif <m.dornseif@hudora.de>
    Tested-by: HudsonAutomation <edv+hudson@hudora.de>
    
    updated examples
    
    Change-Id: Ifdfea838fa7bcc7617b7c1ad969b9b196af2cf38
    Reviewed-on: http://gerrit.hudora.biz/1286
    Tested-by: HudsonAutomation <edv+hudson@hudora.de>
    Reviewed-by: Christian Klein <c.klein@hudora.de>
    Reviewed-by: Philipp Benjamin Koeppchen <b.koeppchen@hudora.de>

---
+++ gaetk2/taskqueues.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+infrastructure.py
+
+Created by Maximillian Dornseif on 2011-01-07.
+Copyright (c) 2011 HUDORA. All rights reserved.
+"""
+
+
+import zlib
+
+from google.appengine.api import taskqueue
+
+
+def taskqueue_add_multi(qname, url, paramlist, **kwargs):
+    """Adds more than one Task to the same Taskqueue/URL.
+
+    tasks = []
+    for kdnnr in kunden.get_changed():
+        tasks.append(dict(kundennr=kdnnr))
+    taskqueue_add_multi('softmq', '/some/path', tasks)
+    """
+
+    tasks = []
+    for params in paramlist:
+        tasks.append(taskqueue.Task(url=url, params=params, **kwargs))
+        # Batch Addition to Taskqueue
+        if len(tasks) >= 50:
+            taskqueue.Queue(name=qname).add(tasks)
+            tasks = []
+    if tasks:
+        taskqueue.Queue(name=qname).add(tasks)
+    #logging.debug(u'%d tasks queued to %s', len(paramlist), url)
+
+
+def taskqueue_add_multi_payload(name, url, payloadlist, **kwargs):
+    """like taskqueue_add_multi() but transmit a json encoded payload instead a query parameter.
+
+    In the Task handler you can get the data via zdata = json.loads(self.request.body)`.
+    See http://code.google.com/appengine/docs/python/taskqueue/tasks.html"""
+
+    import huTools.hujson
+    tasks = []
+    for payload in payloadlist:
+        payload = huTools.hujson.dumps(payload)
+        payload = zlib.compress(payload)
+        tasks.append(taskqueue.Task(url=url, payload=payload, **kwargs))
+        # Patch Addition to Taskqueue
+        if len(tasks) >= 50:
+            taskqueue.Queue(name=name).add(tasks)
+            tasks = []
+    if tasks:
+        taskqueue.Queue(name=name).add(tasks)
+    logging.debug(u'%d tasks queued to %s', len(payloadlist), url)
