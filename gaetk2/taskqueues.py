9cf7b1c039014f3a2159b44c5113e117e3fb3b83
Christian Klein <c.klein@hudora.de>
2017-01-06 09:31:27 +0100

    Datastore-Stuff
    Bugfix für ndb-Instanzen
    
    Funktion reload_obj
    
    Lädt ein Objekt neu aus dem Datastore (ohne Cache)
    
    Bugfix
    
    cleanup
    
    `reload_obj(obj)` db compatible
    
    avoid writing to non-existing properties
    
    enable to add new properties in `write_on_change()`

--- gaetk2/taskqueues.py
+++ gaetk2/taskqueues.py
@@ -4,7 +4,7 @@
 infrastructure.py
 
 Created by Maximillian Dornseif on 2011-01-07.
-Copyright (c) 2011, 2012, 2016 Cyberlogi/HUDORA. All rights reserved.
+Copyright (c) 2011, 2012, 2016, 2017 Cyberlogi/HUDORA. All rights reserved.
 """
 import re
 import zlib
@@ -14,11 +14,14 @@ import google.appengine.ext.deferred.deferred
 from gaetk import compat
 from google.appengine.api import memcache
 from google.appengine.api import taskqueue
+from google.appengine.ext import db
 from google.appengine.ext import deferred
 from google.appengine.ext import ndb
 
 from google.appengine.api import taskqueue
 
+
+# Tasks
 
 def taskqueue_add_multi(qname, url, paramlist, **kwargs):
     """Adds more than one Task to the same Taskqueue/URL.
@@ -61,68 +64,6 @@ def taskqueue_add_multi_payload(name, url, payloadlist, **kwargs):
     logging.debug(u'%d tasks queued to %s', len(payloadlist), url)
 
 
-def query_iterator(query, limit=50):
-    """Iterates over a datastore query while avoiding timeouts via a cursor."""
-    cursor = None
-    while True:
-        bucket, cursor, more_objects = compat.xdb_fetch_page(query, limit, start_cursor=cursor)
-        if not bucket:
-            break
-        for entity in bucket:
-            yield entity
-        if not more_objects:
-            break
-
-
-def copy_entity(entity, **kwargs):
-    """Copy entity"""
-    klass = type(entity)
-    properties = dict((key, value.__get__(entity, klass)) for (key, value) in klass.properties().iteritems())
-    properties.update(**kwargs)
-    return klass(**properties)
-
-
-def flush_ndb_cache(instance):
-    """
-    Flush memcached ndb instance
-
-    Especially usefull if you mix (old) db and ndb for a model.
-    """
-    key = ndb.Context._memcache_prefix + str(instance.key())
-    memcache.delete(key)
-
-
-def write_on_change(model, key, data, flush_cache=False):
-    """Schreibe (nur) die geänderten Daten in den Datastore"""
-
-    key_name = data[key]
-    obj = compat.get_by_id_or_name(model, key_name)
-    if obj is None:
-        obj = model(key=compat.xdb_create_key(model, key_name), **data)
-        obj.put()
-        return obj
-
-    changed, obj = write_on_change_instance(obj, data)
-    if flush_cache and changed:
-        flush_ndb_cache(obj)
-
-    return obj
-
-
-def write_on_change_instance(obj, data):
-    """Schreibe Instanz mit geänderten Daten in Datastore"""
-
-    dirty = False
-    for key, value in data.iteritems():
-        if value != getattr(obj, key, None):
-            setattr(obj, key, value)
-            dirty = True
-    if dirty:
-        obj.put()
-
-    return dirty, obj
-
-
 def defer(obj, *args, **kwargs):
     """Defers a callable for execution later.
 
@@ -150,3 +91,75 @@ def defer(obj, *args, **kwargs):
     kwargs["_target"] = kwargs.pop("_target", 'workers')
     kwargs["_queue"] = kwargs.pop("_queue", 'workersq')
     return deferred.defer(obj, *args, **kwargs)
+
+
+# Datastore
+
+def query_iterator(query, limit=50):
+    """Iterates over a datastore query while avoiding timeouts via a cursor."""
+    cursor = None
+    while True:
+        bucket, cursor, more_objects = compat.xdb_fetch_page(query, limit, start_cursor=cursor)
+        if not bucket:
+            break
+        for entity in bucket:
+            yield entity
+        if not more_objects:
+            break
+
+
+def copy_entity(e, **extra_args):
+    """Copy entity but change values in kwargs."""
+    # see https://stackoverflow.com/a/2712401
+    klass = e.__class__
+    props = dict((v._code_name, v.__get__(e, klass)) for v in klass._properties.itervalues() if type(v) is not ndb.ComputedProperty)
+    props.update(extra_args)
+    return klass(**props)
+
+def write_on_change(model, key, data, flush_cache=False):
+    """Schreibe (nur) die geänderten Daten in den Datastore."""
+
+    key_name = data[key]
+    obj = compat.get_by_id_or_name(model, key_name)
+    if obj is None:
+        obj = model(key=compat.xdb_create_key(model, key_name), **data)
+        obj.put()
+        return obj
+
+    changed, obj = write_on_change_instance(obj, data)
+    if flush_cache and changed:
+        flush_ndb_cache(obj)
+
+    return obj
+
+
+def write_on_change_instance(obj, data):
+    """Schreibe Instanz mit geänderten Daten in Datastore."""
+
+    properties = compat.xdb_properties(obj)
+    dirty = False
+    for key, value in data.iteritems():
+        if value != getattr(obj, key, None):
+            setattr(obj, key, value)
+            dirty = True
+    if dirty:
+        obj.put()
+
+    return dirty, obj
+
+def flush_ndb_cache(instance):
+    """
+    Flush memcached ndb instance.
+
+    Especially usefull if you mix (old) db and ndb for a model.
+    """
+    key = ndb.Context._memcache_prefix + compat.xdb_str_key(compat.xdb_key(instance))
+    memcache.delete(key)
+
+
+def reload_obj(obj):
+    """Objekt ohne Cache neu laden."""
+    if compat.xdb_is_ndb(obj):
+        return obj.key.get(use_cache=False, use_memcache=False)
+    else:
+        return db.get(obj.key())
