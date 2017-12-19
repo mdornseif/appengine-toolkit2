87253e82c82ea04754998fafb51b943cf9ef1997
Maximillian Dornseif <md@hudora.de>
2017-12-20 00:31:10 +0100

    deprecate abort(), redirect(), redirect_to()

--- gaetk2/handlers/legacy.py
+++ gaetk2/handlers/legacy.py
@@ -16,6 +16,32 @@ from .mixins import PaginateMixin
 class OldGaetkMixin(PaginateMixin, BootstrapMixin):
     """Emulate gaetk.handler methods which have been removed in gaetk2."""
 
+    def abort(self, code, *args, **kwargs):
+        """Emulate ambigus Method."""
+        warnings.warn(
+            "abort() hides control flow. `raise WSGIHTTPException` instead.",
+            DeprecationWarning, stacklevel=2)
+        return super(OldGaetkMixin, self).abort(code, *args, **kwargs)
+
+    def redirect(self, uri, permanent=False, abort=False, code=None,
+                 body=None):
+        """Emulate ambigus Method."""
+        warnings.warn(
+            "redirect() hides control flow. `raise HTTPRedirection` instead.",
+            DeprecationWarning, stacklevel=2)
+        return super(OldGaetkMixin, self).redirect(
+            uri, permanent=permanent, abort=abort, code=code, body=body)
+
+    def redirect_to(self, _name, _permanent=False, _abort=False, _code=None,
+                    _body=None, *args, **kwargs):
+        """Emulate ambigus Method."""
+        warnings.warn(
+            "redirect_to() hides control flow. `raise HTTPRedirection` instead.",
+            DeprecationWarning, stacklevel=2)
+        return super(OldGaetkMixin, self).redirect_to(
+            _name, _permanent=_permanent, _abort=_abort, _code=_code,
+            _body=_body, *args, **kwargs)
+
     def is_admin(self):
         """Emulate ambigus Method."""
         warnings.warn(
