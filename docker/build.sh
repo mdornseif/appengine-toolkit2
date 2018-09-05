18a53a4b1344e35ca3e7ab2065ebebfc1aa874a2
Maximillian Dornseif <md@hudora.de>
2018-09-05 13:07:37 +0200

    finetuned Docker image

--- docker/build.sh
+++ docker/build.sh
@@ -4,7 +4,7 @@ cat ../requirements-dev.txt ../requirements-lib.txt | sort -u > requirements-cac
 docker build --tag gae .
 docker tag gae mdornseif/gae:latest
 docker push mdornseif/gae:latest
-# docker tag gae mdornseif/gae:stable
-# docker push mdornseif/gae:stable
-docker run --rm --env-file env.list --env CHECKOUT_KEY="`cat ~/.ssh/id_github_key`" -ti mdornseif/gae bash
-docker run --rm -ti mdornseif/gae:stable bash
+docker tag gae mdornseif/gae:stable
+docker push mdornseif/gae:stable
+# docker run --rm --env-file env.list --env CHECKOUT_KEY="`cat ~/.ssh/id_github_key`" -ti mdornseif/gae bash
+# docker run --rm -ti mdornseif/gae:stable bash
