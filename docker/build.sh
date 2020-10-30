1774ed7250dd6e4718f706b1130c5ca0605fcc2d
Dr. Maximillian Dornseif <md@foxel.org>
2020-10-30 14:39:24 +0100

    Aktuallisiertes Docker Image

--- docker/build.sh
+++ docker/build.sh
@@ -3,8 +3,9 @@ cp ../requirements-dev.txt .
 cat ../requirements-dev.txt ../requirements-lib.txt | sort -u > requirements-cache.txt
 docker build --tag gae .
 # docker push mdornseif/gae:latest
-docker tag gae mdornseif/gae:`date +'%Y.%b'`
-docker push mdornseif/gae:`date +'%Y.%b'`
+docker tag gae mdornseif/gae:`date +'%Y.%b'`-test
+docker push mdornseif/gae:`date +'%Y.%b'`-test
+echo mdornseif/gae:`date +'%Y.%b'`-test
 # docker tag gae mdornseif/gae:stable
 # docker push mdornseif/gae:stable
 # docker run --rm --env-file env.list --env CHECKOUT_KEY="`cat ~/.ssh/id_github_key`" -ti mdornseif/gae bash
